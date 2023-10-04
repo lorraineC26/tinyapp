bd09e4894a5e6700c35f4f15328b0400bde1a119
lorraineC26 <lorraine.quinyee@gmail.com>
2023-10-03 21:12:51 -0400

    nodemon used

--- express_server.js
+++ express_server.js
@@ -4,6 +4,10 @@ const PORT = 8080; // default port 8080
 
 app.set('view engine', 'ejs');
 
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}!`);
+});
+
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
@@ -54,6 +58,3 @@ app.get('/Hello', (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 
-app.listen(PORT, () => {
-  console.log(`Example app listening on port ${PORT}!`);
-});
